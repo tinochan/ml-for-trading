edc833217ee6e50c0b3b591d5710ab7032177927
tinochan <tinokwchan@gmail.com>
2016-04-03 19:49:42 +0800

    Create PlotHighPrice.py
    Initial Commit

---
+++ 01-01_Reading_Plotting/PlotHighPrice.py
@@ -0,0 +1,16 @@
+"""Plot High prices for IBM"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def test_run():
+    df = pd.read_csv("data/IBM.csv")
+    # TODO: Your code here
+    plt.title('IBM High')
+    plt.ylabel('$')
+    plt.plot(df['High'])
+    plt.show()  # must be called to show plots
+
+
+if __name__ == "__main__":
+    test_run()
