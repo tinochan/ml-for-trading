2337c7e086bf5fe2888cc3318fe4ea09040575f0
tino.chan <tino@tinochan.com>
2016-04-04 13:44:18 +0800

    Edit: 01-01_Reading_Plotting

--- 01-01_Reading_Plotting/PlotHighPrice.py
+++ 01-01_Reading_Plotting/PlotHighPrice.py
@@ -4,13 +4,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def test_run():
-    df = pd.read_csv("data/IBM.csv")
+    df = pd.read_csv("../data/IBM.csv")
     # TODO: Your code here
+
+    plt.figure(1)
+
+    #high
+    plt.subplot(121)
     plt.title('IBM High')
     plt.ylabel('$')
-    plt.plot(df['High']) #Or df['High'].plot()
-    plt.show()  # must be called to show plots
+    plt.plot(df['High']) #Or df['High'].plot() to show the graph immediately
 
+    #close & adj close
+    plt.subplot(122)
+    plt.title('IBM Close & Adj Close')
+    plt.ylabel('$')
+    plt.plot(df[['Close','Adj Close']])
+    plt.grid(True)
+
+    plt.show()  # must be called to show plots
 
 if __name__ == "__main__":
     test_run()
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # ml-for-trading
-Source Code of the Udacity course Machine Learning for Trading by Tucker Balch
+Source Code of the Udacity course Machine Learning for Trading by Tucker Balch. Have included extra examples into the demos.
 
 [Course web site](https://www.udacity.com/course/machine-learning-for-trading--ud501)
+
+You may find installing [Anaconda](https://docs.continuum.io/anaconda/install) a convenience way to get all the necessary libraries to run these demos.
+
+To begin with, clone and import this project to IntelliJ.
