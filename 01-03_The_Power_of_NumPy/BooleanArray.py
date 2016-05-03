d63a652c404130f3d4981b21d5acbe12fb9e2d04
tino.chan <tino@tinochan.com>
2016-05-03 23:09:45 +0800

    Add:  01-03_The_Power_of_NumPy Boolean or mask index arrays

---
+++ 01-03_The_Power_of_NumPy/BooleanArray.py
@@ -0,0 +1,18 @@
+"""Boolean or mask index arrays"""
+
+import numpy as np
+
+def test_run():
+    a = np.array([(20,25,10,23,26,32,10,5,0),(0,2,50,20,0,1,28,5,0)])
+    print a
+
+    #calculating mean
+    mean = a.mean()
+    print mean
+
+    #masking
+    print a[a<mean]
+
+
+if __name__ == "__main__":
+    test_run()
